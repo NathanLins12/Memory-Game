dcd91a1ab71cd8e0ee8f08c02d7214ace94d7ccc
Nathan Lins <nathan.lins012@gmail.com>
2023-11-29 19:27:51 -0300

    add button navigation to rank

--- scripts/newGame.js
+++ scripts/newGame.js
@@ -1,7 +1,3 @@
-const inputName = document.querySelector(".inputName")
-const newGameButton = document.querySelector(".newGameButton")
-const newGameForm = document.querySelector(".newGameForm")
-
 function validateInput(event) {
     if (event.target.value.length >= 3) {
         newGameButton.removeAttribute("disabled")
@@ -18,5 +14,15 @@ function hadleSubmitNewGame(event) {
     window.location.href = "pages/cards.html"
 }
 
+function navigationToRank() {
+    window.location.href = "pages/rank.html"
+}
+
+const inputName = document.querySelector(".inputName")
+const newGameButton = document.querySelector(".newGameButton")
+const newGameForm = document.querySelector(".newGameForm")
+const buttonRank = document.querySelector(".buttonRank")
+
 inputName.addEventListener("input", validateInput)
 newGameForm.addEventListener("submit", hadleSubmitNewGame)
+buttonRank.addEventListener("click", navigationToRank)
