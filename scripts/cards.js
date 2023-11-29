cc5e3ee017d26840de30d95278aaecc130758b8c
Nathan Lins <nathan.lins012@gmail.com>
2023-11-28 21:10:51 -0300

    add fdunctions setStartTimer and checkGameWin

--- scripts/cards.js
+++ scripts/cards.js
@@ -58,7 +58,8 @@ function checkMatchCards() {
       secondCard.classList.add("disableCard");
       firstCard = "";
       secondCard = "";
-    }, 500)
+      checkGameWin()
+    }, 500);
   } else {
     setTimeout(() => {
       firstCard.classList.remove("flipCard");
@@ -66,6 +67,31 @@ function checkMatchCards() {
       firstCard = "";
       secondCard = "";
     }, 500);
+  }
+}
+
+function checkGameWin() {
+  const disabledCards = document.querySelectorAll(".disableCard");
+  if (disabledCards.length === 2) {
+    clearInterval(finishTimerInterval);
+
+    const userData = {
+      name: storagePlayerName,
+      time: timer.textContent,
+    };
+
+    const storageRank = JSON.parse(localStorage.getItem("@memoryGame:rank"));
+    
+    if (storageRank) {
+      console.log(storageRank);
+      const rankData = [...storageRank, userData];
+
+    } else {
+      localStorage.setItem("@memoryGame:rank", JSON.stringify([userData]))
+    }
+
+
+    alert(`Parabéns ${storagePlayerName}, você venceu com o tempo de ${timer.innerHTML}!`);
   }
 }
 
@@ -90,13 +116,25 @@ function clickFlipCard() {
   });
 }
 
+function setStartTimer() {
+  finishTimerInterval = setInterval(() => {
+    const dateNow = new Date();
+    const dateDiff = new Date(dateNow - initialDateTimer);
+    const minutes = String(dateDiff.getMinutes()).padStart("2", "0");
+    const seconds = String(dateDiff.getSeconds()).padStart("2", "0");
+
+    timer.innerHTML = `${minutes}:${seconds}`;
+  }, 1000);
+}
+
 const playerName = document.querySelector(".playerName");
 const backButton = document.querySelector(".backButton");
 const gridCards = document.querySelector(".gridCards");
+const timer = document.querySelector(".timer");
 
-const StoragePlayerName = localStorage.getItem("@memory_game:playerName");
+const storagePlayerName = localStorage.getItem("@memory_game:playerName");
 
-playerName.innerHTML = StoragePlayerName;
+playerName.innerHTML = storagePlayerName;
 
 backButton.addEventListener("click", backPage);
 
@@ -105,3 +143,7 @@ createCards();
 let firstCard = "";
 let secondCard = "";
 clickFlipCard();
+
+const initialDateTimer = new Date();
+let finishTimerInterval;
+setStartTimer();
